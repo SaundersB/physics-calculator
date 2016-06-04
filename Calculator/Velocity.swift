fa97d495aca5749b9a110d240fcde6f0df8b9122
SaundersB <speakeasys@gmail.com>
2016-06-04 10:53:56 -0700

    Adding each equation to each respective storyboard. Also, adding the computation of initial velocity.

--- Calculator/Velocity.swift
+++ Calculator/Velocity.swift
@@ -13,15 +13,61 @@ class Velocity: UIViewController {
     
     @IBOutlet weak var velocity_header: UILabel!
     
+    @IBOutlet weak var initial_velocity_label: UILabel!
+    @IBOutlet weak var time_label: UILabel!
+    @IBOutlet weak var acceleration_label: UILabel!
+    
+    @IBOutlet weak var initial_velocity_field: UITextField!
+    @IBOutlet weak var time_field: UITextField!
+    @IBOutlet weak var acceleration_field: UITextField!
+    
+    @IBOutlet weak var displacement_field: UITextField!
+    
+    @IBOutlet weak var clear_button: UIButton!
+    @IBOutlet weak var calculate_button: UIButton!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         velocity_header.textAlignment = NSTextAlignment.Center;
         velocity_header.font = UIFont (name: "Helvetica Neue", size: 30)
-
+        
+        initial_velocity_field.text = "0.0"
+        time_field.text = "0.0"
+        acceleration_field.text = "0.0"
+        displacement_field.text = "0.0"
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func compute_velocity(sender: AnyObject) {
+        // v0 = x/t - ½at
+        var initial_velocity = 0.0
+        let time = Double(time_field.text!)
+        let acceleration = Double(acceleration_field.text!)
+        let displacement = Double(displacement_field.text!)
+        
+        let alert = UIAlertController(title: "Alert", message: "Please input at least two values", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        if (time == 0 && acceleration == 0 || time == 0 && displacement == 0 || acceleration == 0 && displacement == 0){
+            alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        initial_velocity = (displacement! / time!) - (0.5 * acceleration! * time!)
+        
+        initial_velocity_field.text = String(initial_velocity)
+    }
+    
+    @IBAction func clear_results(sender: AnyObject) {
+        
+        initial_velocity_field.text = ""
+        time_field.text = ""
+        acceleration_field.text = ""
+        displacement_field.text = ""
     }
 }
